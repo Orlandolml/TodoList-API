a93e722f2674150ea024be6a36e25988ecee2aa3
Orlandolml <tortonorlando.omg@gmail.com>
2021-05-26 15:44:39 -0500

    config

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => res.json({ greeting: "hello world!" }));
 app.use("/", routes);
 
 app.use(errorHandler);
