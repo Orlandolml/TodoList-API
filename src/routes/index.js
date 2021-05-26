3ade5b0cd8ad5e15d7626f384392c91ca17b3233
Orlandolml <tortonorlando.omg@gmail.com>
2021-05-26 16:34:11 -0500

    config

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const users = require("./users");
 const todos = require("./todos");
 const scheduledTasks = require("./scheduledTasks");
 
-router.use("/", (req, res) => res.json("Welcome to Taskman api"));
 router.use("/users", users);
-router.use("/scheduledTasks", scheduledTasks);
 router.use("/todos", todos);
+router.use("/scheduledTasks", scheduledTasks);
+router.get("/", (req, res) => res.json("Welcome to Taskman api"));
 
 module.exports = router;
