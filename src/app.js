c7f8c766e8da92678dd7a9cb9266e80710dd9cdd
Orlandolml <tortonorlando.omg@gmail.com>
2021-05-26 00:33:29 -0500

    port deleted

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ app.use(
   myConnection(
     mysql,
     {
-      port: process.env.PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
