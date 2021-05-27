e3d2be002605bd42e4399034aae142a4a15fe617
Orlandolml <tortonorlando.omg@gmail.com>
2021-05-27 11:30:32 -0500

    config

--- src/app.js
+++ src/app.js
@@ -2,36 +2,29 @@ const cors = require("cors");
 const mysql = require("mysql");
 const morgan = require("morgan");
 const routes = require("./routes");
-const myConnection = require("express-myconnection");
-
 const express = require("express");
-const app = express();
-
 const errorHandler = require("./middlewares/errorHandler");
+
+const app = express();
 
 app.set("port", process.env.PORT);
 
-// Middlewares
+// Middlewares and Mysql connection
+mysql
+  .createConnection({
+    port: process.env.PORT,
+    user: "b2f30ce7f56fdb",
+    password: "28e3baa4",
+    host: "us-cdbr-east-03.cleardb.com",
+    database: "heroku_a9576ac2ef67aab",
+  })
+  .connect();
+
 app.use(cors());
 app.use(morgan("dev"));
-app.use(
-  myConnection(
-    mysql,
-    {
-      port: process.env.PORT,
-      user: "b2f30ce7f56fdb",
-      password: "28e3baa4",
-      host: "us-cdbr-east-03.cleardb.com",
-      database: "heroku_a9576ac2ef67aab",
-    },
-    "single"
-  )
-);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/", routes);
-
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
