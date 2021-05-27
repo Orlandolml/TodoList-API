ed5f3bf8b75b1e5f843a043b82c116b93196ea22
Orlandolml <tortonorlando.omg@gmail.com>
2021-05-27 18:08:09 -0500

    config

--- src/app.js
+++ src/app.js
@@ -9,32 +9,34 @@ const app = express();
 
 app.set("port", process.env.PORT);
 
-let connection = mysql.createConnection({
-  port: 3306,
-  user: "uyzg3ncerjmbpvgk",
-  password: "j1WD96hFbw2CxUXkJynM",
-  host: "bqbvr2fmf5brtjqhuaky-mysql.services.clever-cloud.com",
-  database: "bqbvr2fmf5brtjqhuaky",
-});
+app.use(
+  myConnection(
+    mysql,
+    {
+      port: 3306,
+      user: "uyzg3ncerjmbpvgk",
+      password: "j1WD96hFbw2CxUXkJynM",
+      host: "bqbvr2fmf5brtjqhuaky-mysql.services.clever-cloud.com",
+      database: "bqbvr2fmf5brtjqhuaky",
+    },
+    "single"
+  )
+);
 
 // Middlewares and Mysql connection
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connection.connect();
+let reqConnection = connection.connect();
 
-app.get("/", (req, res) => {
-  connection.query("SELECT * FROM users", (error, user) => {
-    if (error) {
-      res.json(error);
-    }
-    res.json({ success: true, user: user[0] });
-  });
-});
-//app.use("/", routes);
+app.use("/", routes);
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is listening on port ${process.env.PORT}`)
 );
+
+module.exports = {
+  connection: reqConnection,
+};
--- src/routes/users/post/createUser.js
+++ src/routes/users/post/createUser.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const pool = require("../../../pool");
+const { connection } = require("../../../app");
 
 module.exports = async (req, res, next) => {
   try {
@@ -8,27 +8,37 @@ module.exports = async (req, res, next) => {
     let password = userBody.password;
     userBody.password = await bcrypt.hash(password, 10);
 
-    pool.query("INSERT INTO users set ?", [userBody], (error, insertedUser) => {
+    req.getConnection((error, conn) => {
       if (error) {
         return next(error);
       } else {
-        pool.query(
-          "SELECT * FROM users WHERE id = ?",
-          [insertedUser.insertId],
-          (error, user) => {
+        conn.query(
+          "INSERT INTO users set ?",
+          [userBody],
+          (error, insertedUser) => {
             if (error) {
               return next(error);
             } else {
-              res.json({
-                success: true,
-                token: jwt.sign(
-                  { userId: user[0].id },
-                  process.env.JWT_SECRET_KEY,
-                  {
-                    expiresIn: 60 * 60 * 6,
+              conn.query(
+                "SELECT * FROM users WHERE id = ?",
+                [insertedUser.insertId],
+                (error, user) => {
+                  if (error) {
+                    return next(error);
+                  } else {
+                    res.json({
+                      success: true,
+                      token: jwt.sign(
+                        { userId: user[0].id },
+                        process.env.JWT_SECRET_KEY,
+                        {
+                          expiresIn: 60 * 60 * 6,
+                        }
+                      ),
+                    });
                   }
-                ),
-              });
+                }
+              );
             }
           }
         );
