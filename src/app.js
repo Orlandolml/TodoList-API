e2bd21d841275c7cac924b0a128b5587693d3e3f
Orlandolml <tortonorlando.omg@gmail.com>
2021-05-28 14:37:59 -0500

    config

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(
 );
 
 // Middlewares and Mysql connection
-app.use(cors("http://localhost:3000"));
+app.use(cors({ origin: "http:localhost:300" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
